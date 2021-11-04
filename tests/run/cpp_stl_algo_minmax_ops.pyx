b56ae82b8b77d53910597d8010b96f2f40747994
jhelgert <jonathan.helgert@posteo.de>
2021-11-04 11:47:37 +0100

    bla

--- tests/run/cpp_stl_algo_minmax_ops.pyx
+++ tests/run/cpp_stl_algo_minmax_ops.pyx
@@ -24,6 +24,26 @@ def test_min_element(vector[int] v):
     cdef vector[int].iterator it = min_element(v.begin(), v.end())
     return deref(it)
 
+def test_min_element_with_pred(vector[int] v):
+    """
+    Test min_element with binary predicate.
+
+    >>> test_min_element_with_pred([0, 1, 2, 3, 4, 5])
+    0
+    """
+    cdef vector[int].iterator it = min_element(v.begin(), v.end(), less)
+    return deref(it)
+
+def test_min_element_with_exec(vector[int] v):
+    """
+    Test min_element with execution policy.
+
+    >>> test_min_element_with_exec([0, 1, 2, 3, 4, 5])
+    0
+    """
+    cdef vector[int].iterator it = min_element(seq, v.begin(), v.end())
+    return deref(it)
+
 def test_max_element(vector[int] v):
     """
     Test max_element.
@@ -53,16 +73,6 @@ def test_max_element_with_exec(vector[int] v):
     """
     cdef vector[int].iterator it = max_element(seq, v.begin(), v.end())
     return deref(it)
-
-# def test_max_element_with_exec_and_pred(vector[int] v):
-#     """
-#     Test max_element with execution policy and binary predicate.
-
-#     >>> test_max_element_with_exec_and_pred([0, 1, 2, 3, 4, 5])
-#     5
-#     """
-#     cdef vector[int].iterator it = max_element(seq, v.begin(), v.end(), less)
-#     return deref(it)
 
 def test_minmax(int a, int b):
     """
@@ -102,6 +112,16 @@ def test_minmax_element_with_pred(vector[int] v):
     [0, 5]
     """
     cdef pair[vector[int].iterator, vector[int].iterator] p = minmax_element(v.begin(), v.end(), less)
+    return [deref(p.first), deref(p.second)]
+
+def test_minmax_element_with_exec(vector[int] v):
+    """
+    Test minmax_element with execution policy.
+
+    >>> test_minmax_element_with_exec([0, 1, 2, 3, 4, 5])
+    [0, 5]
+    """
+    cdef pair[vector[int].iterator, vector[int].iterator] p = minmax_element(seq, v.begin(), v.end())
     return [deref(p.first), deref(p.second)]
 
 def test_clamp(int v, int lo, int hi):
